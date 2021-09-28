cb3e1c92044f971b8e1c6d37214bbfdcb945a7a2
paparoc <paulparoczai@gmail.com>
2021-09-27 18:11:37 -0700

    remove other instrument classes

--- scripts/parts.js
+++ scripts/parts.js
@@ -55,31 +55,6 @@ class Piece {
 
 }
 
-class FilterTickSection extends Piece {
-
-    constructor( piece ){
-
-        super();
-
-        this.piece = piece;
-
-    }
-
-    load(){
-
-        this.fT1 = new FilterTick( this.piece );
-        this.fT1.load();
-
-    }
-
-    start( rate , startTime , stopTime ){
-
-        this.fT1.start( rate , startTime , stopTime );
-
-    }
-
-}
-
 class NoisePanSection extends Piece {
 
     constructor( piece ){
@@ -108,31 +83,6 @@ class NoisePanSection extends Piece {
         this.nP1.start( 12 , [ 5000 , 8000 ] , 0 , piece.globalNow + 30 );
         this.nP2.start( 12 , [ 200 , 500 ] , 0 ,   piece.globalNow + 30 );
         this.nP3.start( 12 , [ 1000 , 3000 ] , 0 , piece.globalNow + 30 );
-
-    }
-
-}
-
-class NoiseTickSection extends Piece {
-
-    constructor( piece ){
-
-        super();
-
-        this.piece = piece;
-
-    }
-
-    load(){
-
-        this.nT1 = new NoiseTick( this.piece );
-        this.nT1.load( 0.5 );
-
-    }
-
-    start( rate , startTime , stopTime ){
-
-        this.nT1.start( rate , startTime , stopTime );
 
     }
 
@@ -184,116 +134,6 @@ class NoisePan extends NoisePanSection {
                 this.pan.setPositionAtTime( randomArrayValue( [ randomFloat( -1 , -0.6 ) , randomFloat( 0.6 , 1 ) ] ) , time );
                 this.filter.biquad.frequency.setValueAtTime( randomFloat( filterRange[ 0 ] , filterRange[ 1 ] ) , time );
                 this.noise.output.gain.setValueAtTime( randomArrayValue( [ 0 , 1 , 1 , 1 , 1 , 1 ] ) , time );
-
-            }
-
-        }
-
-        this.noise.stopAtTime( stopTime );
-
-    }
-
-}
-
-class FilterTick extends FilterTickSection {
-
-    constructor( piece ){
-
-        super();
-
-        this.output = new MyGain( 1 );
-
-        this.output.connect( piece.masterGain );
-
-    }
-
-    load(){
-
-        this.tick = new MyBuffer2( 1 , 1 , audioCtx.sampleRate );
-        this.tick.sawtooth( 8 ).add( 0 );
-        this.tick.loop = true;
-
-        this.filter = new MyBiquad( 'bandpass' , 300 , 1 );
-
-        this.pan = new MyPanner2( -1 );
-
-        this.tick.connect( this.filter );
-        this.filter.connect( this.pan );
-        this.pan.connect( this.output );
-    }
-
-    start( rate , startTime , stopTime ){
-
-        this.tick.playbackRate = rate;
-        this.tick.startAtTime( startTime );
-        let time = 0;
-
-        for( let i = 0 ; i < 1000 ; i++ ){
-
-            time = startTime + ( i / rate );
-
-            if( time < stopTime ){
-
-                this.filter.biquad.frequency.setValueAtTime( randomFloat( 100 , 5000 ) , time );
-                this.pan.setPositionAtTime( randomArrayValue( [ randomFloat( -1 , -0.6 ) , randomFloat( 0.6 , 1 ) ] ) , time );
-                this.tick.output.gain.setValueAtTime( randomArrayValue( [ 0 , 1 , 1 , 1 , 1 , 1 ] ) , time );
-
-            }
-
-        }
-
-        this.tick.stopAtTime( stopTime );
-
-    }
-
-}
-
-class NoiseTick extends NoiseTickSection {
-
-    constructor( piece ){
-
-        super();
-
-        this.output = new MyGain( 1 );
-
-        this.output.connect( piece.masterGain );
-
-    }
-
-    load( gainVal ){
-
-        this.noise = new MyBuffer2( 1 , 1 , audioCtx.sampleRate );
-        this.noise.noise().fill(0);
-        this.noise.ramp( 0.01 , 0.02 , 0 , 1 , 0.1 , 0.1 ).multiply( 0 );
-        this.noise.loop = true;
-
-        this.filter = new MyBiquad( 'bandpass' , 0 , 1 );
-
-        this.pan = new MyPanner2( -1 );
-
-        this.noise.connect( this.filter );
-        this.filter.connect( this.pan );
-        this.pan.connect( this.output );
-
-        this.output.gain.gain.value = gainVal;
-
-    }
-
-    start( rate , startTime , stopTime ){
-
-        this.noise.playbackRate = rate;
-        this.noise.startAtTime( startTime );
-        let time = 0;
-
-        for( let i = 0 ; i < 1000 ; i++ ){
-
-            time = startTime + ( i / rate );
-
-            if( time < stopTime ){
-
-                this.pan.setPositionAtTime( randomArrayValue( [ randomFloat( -1 , -0.6 ) , randomFloat( 0.6 , 1 ) ] ) , time );
-                this.filter.biquad.frequency.setValueAtTime( randomInt( 7000 , 10000 ) , time );
-                this.noise.output.gain.setValueAtTime( randomArrayValue( [ 0 , 1 ] ) , time );
 
             }
 
