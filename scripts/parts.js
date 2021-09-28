cae4bf96b672fd3db7f83c064cfdae54581255a8
paparoc <paulparoczai@gmail.com>
2021-09-28 16:06:48 -0700

    test complex shapes for frequency filter oscillator

--- scripts/parts.js
+++ scripts/parts.js
@@ -28,16 +28,16 @@ class Piece {
         const fund = 300;
 
         // startTime , fund , centerFrequency , bandwidth , oscillationRate , noiseRate , gain
-        this.rC1 = new RampingConvolver( this.globalNow , fund , 2000 , 1000 , 0.25 , 0.25 , 8 );
-        this.rC2 = new RampingConvolver( this.globalNow , fund , 5000 , 3000 , 0.25 , 1 , 2 );
-        this.rC3 = new RampingConvolver( this.globalNow , fund , 800 ,  500  , 1 , 5 , 1 );
-        this.rC4 = new RampingConvolver( this.globalNow , fund , 5000 , 3500 , 0.1 , 0.25 , 4 );
+        this.rC1 = new RampingConvolver( this.globalNow , fund , 5000 , 4500 , 0.125 , 0.25 , 8 );
+        // this.rC2 = new RampingConvolver( this.globalNow , fund , 5000 , 3000 , 0.25 , 1 , 2 );
+        // this.rC3 = new RampingConvolver( this.globalNow , fund , 800 ,  500  , 1 , 5 , 1 );
+        // this.rC4 = new RampingConvolver( this.globalNow , fund , 5000 , 3500 , 0.1 , 0.25 , 4 );
 
         this.cGain.connect( this.rC1.input );
         this.rC1.output.connect( this.masterGain );
 
-        this.cGain.connect( this.rC2.input );
-        this.rC2.output.connect( this.masterGain );
+        // this.cGain.connect( this.rC2.input );
+        // this.rC2.output.connect( this.masterGain );
 
         // this.cGain.connect( this.rC3.input );
         // this.rC3.output.connect( this.masterGain );
@@ -138,10 +138,12 @@ class RampingConvolver{
         this.nF = new MyBiquad( 'bandpass' , centerFrequency , 2 );
 
         this.nO = new MyBuffer2( 1 , 1 , audioCtx.sampleRate );
-        this.nO.sine( 1 , 1 ).fill( 0 );
+        this.nO.fm( 3 , 5 , 1 ).fill( 0 );
         this.nO.playbackRate = oscillationRate;
         this.nO.loop = true;
         this.nO.startAtTime( startTime );
+
+        bufferGraph( this.nO.buffer );
 
         this.nOG = new MyGain( bandwidth );
 
