7e924eff0fceb074cf90dc8779a0c00735dfabe5
paparoc <paulparoczai@gmail.com>
2021-09-27 19:42:24 -0700

    remove z

--- scripts/parts.js
+++ scripts/parts.js
@@ -25,8 +25,11 @@ class Piece {
         // RAMPING CONVOLVER
 
         this.cGain = new MyGain( 1 );
-        this.rC1 = new RampingConvolver( 1 );
-        this.rC2 = new RampingConvolver( -1 );
+        this.rC1 = new RampingConvolver( this.globalNow );
+        this.rC2 = new RampingConvolver( this.globalNow + 10 );
+        this.rC3 = new RampingConvolver( this.globalNow + 20 );
+        this.rC4 = new RampingConvolver( this.globalNow );
+
 
         this.cGain.connect( this.rC1.input );
         this.rC1.output.connect( this.masterGain );
@@ -34,12 +37,16 @@ class Piece {
         this.cGain.connect( this.rC2.input );
         this.rC2.output.connect( this.masterGain );
 
+        this.cGain.connect( this.rC2.input );
+        this.rC3.output.connect( this.masterGain );
+/*
+        this.cGain.connect( this.rC2.input );
+        this.rC4.output.connect( this.masterGain );
+*/
     }
 
     load(){
 
-        this.noisePanSection = new  NoisePanSection( this );
-        this.noisePanSection.load();
 
     }
 
@@ -47,8 +54,6 @@ class Piece {
 
         this.fadeFilter.start(1, 50);
 		this.globalNow = audioCtx.currentTime;
-
-        this.noisePanSection.start();
 
     }
 
@@ -61,111 +66,9 @@ class Piece {
 
 }
 
-class NoisePanSection extends Piece {
-
-    constructor( piece ){
-
-        super();
-
-        this.piece = piece;
-
-    }
-
-    load(){
-
-        this.nP1 = new NoisePan( this.piece );
-        this.nP1.load( 0.125 );
-
-        this.nP2 = new NoisePan( this.piece );
-        this.nP2.load( 0.125 );
-
-        this.nP3 = new NoisePan( this.piece );
-        this.nP3.load( 0.125 );
-
-    }
-
-    start(){
-
-        const rate = 12;
-
-        const filterRangeArray = [
-            [ 5000 , 8000 ] ,
-            [ 200 , 500 ] ,
-            [ 1000 , 3000 ]
-        ]
-
-        shuffle( filterRangeArray );
-
-        this.nP1.start( rate , filterRangeArray[ 0 ] , 0  , piece.globalNow + 30 );
-        this.nP2.start( rate , filterRangeArray[ 1 ] , 10 , piece.globalNow + 30 );
-        this.nP3.start( rate , filterRangeArray[ 2 ] , 15 , piece.globalNow + 30 );
-
-    }
-
-}
-
-class NoisePan extends NoisePanSection {
-
-    constructor( piece ){
-
-        super();
-
-        this.output = new MyGain( 1 );
-
-        // this.output.connect( piece.masterGain );
-
-        this.output.connect( piece.cGain );
-
-    }
-
-    load( gainVal ){
-
-        this.noise = new MyBuffer2( 1 , 1 , audioCtx.sampleRate );
-        this.noise.noise().fill(0);
-        this.noise.ramp( 0.01 , 0.4 , 0 , 1 , 0.1 , 0.1 ).multiply( 0 );
-        this.noise.loop = true;
-
-        this.filter = new MyBiquad( 'bandpass' , 3000 , 1 );
-
-        this.pan = new MyPanner2( -1 );
-
-        this.noise.connect( this.filter );
-        this.filter.connect( this.pan );
-        this.pan.connect( this.output );
-
-        this.output.gain.gain.value = gainVal;
-
-    }
-
-    start( rate , filterRange , startTime , stopTime ){
-
-        this.noise.playbackRate = rate;
-        this.noise.startAtTime( startTime );
-        let time = 0;
-
-        for( let i = 0 ; i < 1000 ; i++ ){
-
-            time = startTime + ( i / rate );
-
-            if( time < stopTime ){
-
-                this.pan.setPositionAtTime( randomArrayValue( [ randomFloat( -1 , -0.6 ) , randomFloat( 0.6 , 1 ) ] ) , time );
-                this.filter.biquad.frequency.setValueAtTime( randomFloat( filterRange[ 0 ] , filterRange[ 1 ] ) , time );
-                this.noise.output.gain.setValueAtTime( randomArrayValue( [ 0 , 1 , 1 , 1 , 1 , 1 ] ) , time );
-
-            }
-
-        }
-
-        this.noise.stopAtTime( stopTime );
-
-    }
-
-}
-
 class RampingConvolver{
 
-    constructor( panValue ){
+    constructor( startTime ){
 
         this.input = new MyGain( 1 );
         this.output = new MyGain( 1 );
@@ -175,20 +78,21 @@ class RampingConvolver{
         this.cAB = new MyBuffer2( 1 , 2 , audioCtx.sampleRate );
 
         const iArray = [ 1 , M2 , M3 , P4 , P5 , M6 , 2 ];
-        const oArray = [ 1 , 0.5 , 0.25 ];
-        const fund = 432 * 2;
+        const oArray = [ 1 , 0.5 , 2 ];
+        const fund = 432 * 1;
 
         let interval = 0;
         let o = 0;
         let p = 0;
 
-        for( let i = 0 ; i < 10 ; i++ ){
+        for( let i = 0 ; i < 20 ; i++ ){
 
             interval = randomArrayValue( iArray );
             o = randomArrayValue( oArray );
             p = randomFloat( 0.1 , 0.9 );
 
-            this.cAB.fm( fund * interval * o , fund * interval * o , 0.25 ).add( 0 );
+            this.cAB.fm( fund * interval * o , fund * interval * o , 0.5 ).add( 0 );
+            this.cAB.constant( 1 / o ).multiply( 0 );
             this.cAB.ramp( p , p + 0.1 , 0.5 , 0.5 , 0.1 , 0.1 ).multiply( 0 );
 
             this.cB.addBuffer( this.cAB.buffer );
@@ -199,32 +103,30 @@ class RampingConvolver{
 
         this.c.setBuffer( this.cB.buffer );
 
-        this.cR = new MyBuffer2( 1 , 1 , audioCtx.sampleRate );
-        this.cR.sawtooth( 1 ).fill( 0 );
-        this.cR.loop = true;
-        this.cR.playbackRate = 0.25;
+        // NOISE
 
-        this.cG = new MyGain( 0 );
+        this.noise = new MyBuffer2( 1 , 1 , audioCtx.sampleRate );
+        this.noise.noise().fill( 0 );
+        this.noise.playbackRate = 0.25;
+        this.noise.loop = true;
+        this.noise.output.gain.value = 0.1;
+        this.noise.startAtTime( startTime );
 
-        this.cIB = new MyBuffer2( 1 , 1 , audioCtx.sampleRate );
-        this.cIB.ramp( 0 , 1 , 0.01 , 0.015 , 0.1 , 8 ).fill( 0 );
-        this.cIB.playbackRate = 0.5;
-        this.cIB.loop = true;
-    
-        this.cIG = new MyGain( 0 );
+        this.noise.connect( this.c );
 
-        this.p = new MyPanner2( panValue );
+        // DELAY
 
-        this.input.connect( this.cIG ); this.cIB.connect( this.cIG.gain.gain );
-        this.cIG.connect( this.c );
-        this.c.connect( this.cG ); this.cR.connect( this.cG.gain.gain );
-        this.cG.connect( this.p );
-        this.p.connect( this.output );
+        this.d = new Effect();
+        this.d.randomEcho();
+        this.d.on();
 
-        this.c.output.gain.value = 14;
+        this.input.connect( this.c );
+        this.c.connect( this.output );
 
-        this.cIB.start();
-        this.cR.start();
+        this.c.connect( this.d );
+        this.d.connect( this.output );
+
+        this.c.output.gain.value = 1;
 
     }
 
